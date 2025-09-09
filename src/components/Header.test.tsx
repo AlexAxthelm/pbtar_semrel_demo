384e85cd51724e1b94a5205b5c3615f05fea449b
Alex Axthelm <aaxthelm@RMI.org>
2025-09-09 16:01:01 +0200

    test(header): remove outdated test

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -19,11 +19,6 @@ describe("Header component", () => {
     expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument();
   });
 
-  it("displays the organization name", () => {
-    renderHeader();
-    expect(screen.getByText("by RMI")).toBeInTheDocument();
-  });
-
   it("renders the navigation links to Home and About pages", () => {
     renderHeader();
     expect(screen.getByText("Scenarios")).toBeInTheDocument();
